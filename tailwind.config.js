20a5fe31e6e81903f2b95ef48819c28c78c67486
Chun-Yu Lo (Andy Lo) <chunyulo@arch.nctu.edu.tw>
2023-05-29 00:41:35 +0800

    reset tailwind.config.js and empty Header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "1280px",
-      xl: "1440px"
-    },
-
-    extend: {
-      colors: {
-        darkGray: "#313538",
-        selected: "#8B572A"
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
